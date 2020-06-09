601e7809a75dfbe1d0a7767cf10f260f09c5cd34
JapnitS <57968393+JapnitS@users.noreply.github.com>
2020-06-09 11:13:59 -0400

    Updated User Schema

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+
+const connectDB = require('./config/db');
 const app = express();
-app.get('/ ',(req,res) => res.send('API running'));
+connectDB();
+
+app.get('/ ', (req, res) => res.send('API running'));
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/posts',require('./routes/api/posts'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile' ));
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>console.log(`Server Started at port ${PORT}`))
+app.listen(PORT, () => console.log(`Server Started at port ${PORT}`))
